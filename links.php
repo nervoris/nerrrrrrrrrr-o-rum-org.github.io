5ff77691a5249f5244893d6caa9c8acb972b14e4
nervoris <222883544+nervoris@users.noreply.github.com>
2025-08-09 09:59:41 +0300

    Update links.php

--- links.php
+++ links.php
@@ -1,22 +1,15 @@
-<?php
-// Just put this at the very top of your file
-header("Location: https://nervorum.org");
-exit();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <meta http-equiv="refresh" content="0; url=https://nervorum.org" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Nervorum LLC</title>
-  <link rel="stylesheet" href="style.css" />
+  <title>Redirecting...</title>
 </head>
 <body>
-  <section class="animated-background">
-    <div id="stars1"></div>
-    <div id="stars2"></div>
-    <div id="stars3"></div>
-  </section>
+  <p>If you’re not redirected automatically, <a href="https://nervorum.org">click here</a>.</p>
+  <script>
+    window.location.href = "https://nervorum.org";
+  </script>
 </body>
 </html>
